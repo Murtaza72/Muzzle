f355aef53e85184ce866de4d73419c47b80fe75a
Murtaza72 <murtazatuta72@gmail.com>
2024-01-22 03:27:21 +0300

    Add Premake build system

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Muzzle"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	startproject "Sandbox"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Muzzle"
+	location "Muzzle"
+	kind "SharedLib"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/**.h",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"MZ_PLATFORM_WINDOWS",
+			"MZ_BUILD_DLL",
+			"_SILENCE_STDEXT_ARR_ITERS_DEPRECATION_WARNING"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "MZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "MZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "MZ_DIST"
+		optimize "on"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	cppdialect "C++17"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/**.h",
+	}
+
+	includedirs
+	{
+		"Muzzle/vendor/spdlog/include",
+		"Muzzle/src"
+	}
+
+	links "Muzzle"
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"MZ_PLATFORM_WINDOWS",
+			"_SILENCE_STDEXT_ARR_ITERS_DEPRECATION_WARNING"
+		}
+
+	filter "configurations:Debug"
+		defines "MZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "MZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "MZ_DIST"
+		optimize "on"
