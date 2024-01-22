415b5c9b06c80797bd3d2eabfa2f18f24d4fcdc4
Murtaza72 <murtazatuta72@gmail.com>
2024-01-22 17:12:17 +0300

    Add Event System

--- Muzzle/src/Muzzle/Application.cpp
+++ Muzzle/src/Muzzle/Application.cpp
@@ -1,4 +1,8 @@
+#include "mzpch.h"
 #include "Application.h"
+
+#include "Muzzle/Events/AppEvent.h"
+#include "Muzzle/Log.h"
 
 namespace Muzzle {
 
@@ -12,6 +16,16 @@ namespace Muzzle {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			MZ_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			MZ_TRACE(e);
+		}
+
 		while (true);
 	}
 }
